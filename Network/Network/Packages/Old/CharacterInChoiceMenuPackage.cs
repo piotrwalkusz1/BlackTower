f152b82b8c99cfba7947568a91f86bcb10ced88c
Piotr Walkusz <piotrwalkusz1@wp.pl>
2014-07-04 12:24:19 +0200

    Start

---
+++ Network/Network/Packages/Old/CharacterInChoiceMenuPackage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkProject
+{
+    public class CharacterInChoiceMenuPackage : INetworkPackage
+    {
+        public string Name { get; set; }
+
+        public virtual void Set(IncomingMessage message)
+        {
+            Name = message.ReadString();
+        }
+
+        public virtual byte[] ToBytes()
+        {
+            OutgoingMessage data = new OutgoingMessage();
+            data.Write(Name);
+            return data.Data.ToArray();
+        }
+    }
+}
---
+++ Network/Network/Packages/Old/OtherPlayerPackage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace NetworkProject
+{
+    public class OtherPlayerPackage : PlayerPackage
+    {
+        public float Rotation { get; set; }
+        public OtherPlayerStatsPackage Stats { get; set; }
+
+        public override void Set(IncomingMessage message)
+        {
+            base.Set(message);
+            Rotation = message.ReadFloat();
+            Stats = message.Read<OtherPlayerStatsPackage>();
+        }
+
+        public override byte[] ToBytes()
+        {
+            byte[] bytes = base.ToBytes();
+
+            OutgoingMessage message = new OutgoingMessage();
+
+            message.Write(bytes);
+            message.Write(Rotation);
+            message.Write(Stats);
+
+            return message.Data.ToArray();
+        }
+    }
+}
