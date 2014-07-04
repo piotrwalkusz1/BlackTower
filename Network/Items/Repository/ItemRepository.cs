3e6a541beec4bc84fa11c300c0ee5da33583a6ee
Piotr Walkusz <piotrwalkusz1@wp.pl>
2014-07-04 15:13:30 +0200

    Test

--- Network/Items/Repository/ItemRepository.cs
+++ Network/Items/Repository/ItemRepository.cs
@@ -22,6 +22,8 @@ namespace NetworkProject.Items.Repository
         public static void LoadItemsFromResources()
         {
             LoadItemsFromResources(Settings.pathToItemsInResources);
+
+            System.Console.WriteLine("a");
         }
 
         public static void LoadItemsFromResources(string pathToItemsInResources)
